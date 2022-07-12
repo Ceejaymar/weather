7da83137da0f27f6fdea4f6267d16849e664ebbd
ceejaymar <martinezcj2@gmail.com>
2022-07-12 15:46:21 -0400

    create loading component

---
+++ src/common/loading/Loading.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const Container = styled.div`
+  padding: 30px;
+`;
+
+const UpMotion = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    background-color: cornflowerblue;
+    transform: translateY(-50px);
+  }
+  100% {
+    transform: translateY(0)
+  }
+`;
+
+const Circle = styled.span`
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  margin: 0 5px;
+  animation: ${UpMotion} 1s infinite;
+  box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.2);
+
+  &:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  &:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+`;
+
+export const Loading = () => (
+  <Container>
+    <Circle />
+    <Circle />
+    <Circle />
+  </Container>
+);
