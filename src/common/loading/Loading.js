8ccf1857a448e33d7d1a69eb5f073c2dcf641ecc
ceejaymar <martinezcj2@gmail.com>
2022-07-13 03:10:08 -0400

    refactor maincard

--- src/common/loading/Loading.js
+++ src/common/loading/Loading.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 
 const Container = styled.div`
@@ -36,10 +37,14 @@ const Circle = styled.span`
   }
 `;
 
-export const Loading = () => (
-  <Container>
+export const Loading = ({ className }) => (
+  <Container className={className}>
     <Circle />
     <Circle />
     <Circle />
   </Container>
 );
+
+Loading.propTypes = {
+  className: PropTypes.string,
+};
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -5,6 +5,7 @@ import { Input } from 'common/input';
 import { Loading } from 'common/loading';
 import { ErrorMessage } from 'common/error';
 import { Units } from 'pages/main/components/unit';
+import { MainCard } from './components/mainCard';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -12,8 +13,6 @@ const Container = styled.div`
   margin-inline: auto;
   padding-inline: 20px;
 `;
-
-const MainContainer = styled.div``;
 
 const Header = styled.h1`
   color: #fff;
@@ -33,66 +32,8 @@ const UnitSearch = styled.div`
   margin: 40px 0 10px 0;
 `;
 
-const TempContainer = styled.div`
-  display: flex;
-`;
-
-const CurrentForcast = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-  color: #fff;
-  font-weight: 500;
-
-  img {
-    height: 150px;
-  }
-`;
-
-const CurrentTemp = styled.div`
-  text-align: center;
-  flex: 1;
-  font-size: clamp(5rem, 5vw, 5rem);
-  color: #ec6e4c;
-  margin: 0;
-  padding-top: 20px;
-
-  span {
-    font-weight: 300;
-  }
-`;
-
-const Side = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 2;
-`;
-const Humidity = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 0;
-`;
-
-const Sun = styled.div`
-  padding: 20px 0;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const InfoDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-
-  span:nth-child(1) {
-    color: rgba(256, 256, 256, 0.6);
-    font-size: clamp(0.7rem, 1vw, 1.5rem);
-  }
-  span:nth-child(2) {
-    color: #fff;
-    font-size: clamp(1rem, 1.5vw, 2rem);
-  }
+const LoadingStyled = styled(Loading)`
+  margin: auto;
 `;
 
 export const Main = () => {
@@ -131,7 +72,6 @@ export const Main = () => {
   return (
     <Container>
       <Navbar setValue={setSearchValue} date={weather?.date} />
-
       <Header>
         Currently in <span>{weather?.name}</span>
       </Header>
@@ -141,62 +81,8 @@ export const Main = () => {
           <Input setValue={setSearchValue} />
         </form>
       </UnitSearch>
-      {(isLoading || !weather) && !error && <Loading />}
-      {!isLoading && weather && <div></div>}
-      <MainContainer>
-        <TempContainer>
-          <CurrentTemp>
-            {weather?.temp}
-            <span>&deg;</span>
-          </CurrentTemp>
-          <CurrentForcast>
-            <img src={weather?.icon} />
-            <span>{weather?.forecast}</span>
-          </CurrentForcast>
-        </TempContainer>
-        <Side>
-          <Humidity>
-            <InfoDiv>
-              <span>Feels like</span>
-              <span>{weather?.feelsLike}&deg;</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>Low</span>
-              <span>{weather?.tempMin}&deg;</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>High</span>
-              <span>{weather?.tempMax}&deg;</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>Humidity</span>
-              <span>{weather?.humidity}%</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>Pressure</span>
-              <span>{weather?.pressure}</span>
-            </InfoDiv>
-          </Humidity>
-          <Sun>
-            <InfoDiv>
-              <span>Sunrise</span>
-              <span>{weather?.sunrise}</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>Sunset</span>
-              <span>{weather?.sunset}</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>Wind Speed</span>
-              <span>{weather?.windSpeed}</span>
-            </InfoDiv>
-            <InfoDiv>
-              <span>Wind Direction</span>
-              <span>{weather?.windDirection}&deg;</span>
-            </InfoDiv>
-          </Sun>
-        </Side>
-      </MainContainer>
+      {(isLoading || !weather) && !error && <LoadingStyled />}
+      {!isLoading && weather && <MainCard weather={weather} />}
       {error && <ErrorMessage>{error}. Try Again</ErrorMessage>}
     </Container>
   );
