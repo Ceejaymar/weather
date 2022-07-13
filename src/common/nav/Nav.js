044c1796822322d9573964f89ce25ea1584d98de
ceejaymar <martinezcj2@gmail.com>
2022-07-12 23:50:01 -0400

    add date to navbar

--- src/common/nav/Nav.js
+++ src/common/nav/Nav.js
@@ -3,25 +3,36 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-  padding-inline: 20vw;
+  padding-inline: 15vw;
+  display: flex;
+  justify-content: space-between;
   text-align: left;
   font-weight: 700;
   font-size: 1.5rem;
+  color: #fff;
+  background-color: #2f3044;
   span {
     font-weight: 500;
   }
 `;
 
-export const Navbar = () => {
+const Date = styled.p`
+  font-weight: 500;
+  font-size: 1.4rem;
+`;
+
+export const Navbar = ({ date }) => {
   return (
     <Nav>
       <p>
         Weather<span>app</span>
       </p>
+      <Date>{date}</Date>
     </Nav>
   );
 };
 
 Navbar.propTypes = {
   setValue: PropTypes.func,
+  date: PropTypes.string.isRequired,
 };
